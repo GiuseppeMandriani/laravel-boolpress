6fd9e63180ad37b0be3556fa40340d359db6785f
GiuseppeMandriani <giuseppe.mandriani@gmail.com>
2021-06-16 19:02:10 +0200

    add migrations

--- database/seeds/PostsTableSeeder.php
+++ database/seeds/PostsTableSeeder.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use App\Post;
+use Illuminate\Support\Str;
+use Faker\Generator as Faker;
 class PostsTableSeeder extends Seeder
 {
     /**
@@ -9,8 +11,27 @@ class PostsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
-        //
+        for ($i=0; $i<20; $i++){
+            // Istanza
+            $new_post = new Post();
+
+
+
+            // Popolazione
+
+            $new_post->title =$faker->text(50);
+            $new_post->slug = Str::slug($new_post->title,'-');
+            $new_post->content = $faker->paragraphs(3, true);
+            $new_post->date = $faker->datetime();
+
+
+
+
+            // Salvataggio
+            $new_post->save();
+
+        }
     }
 }
